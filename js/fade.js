60e0e8bac90445632b552f54785b41d0edc52645
2024040 <86580403+2024040@users.noreply.github.com>
2022-04-21 15:19:59 +0900

    Create fade.js

---
+++ js/fade.js
@@ -0,0 +1,35 @@
+$(function() {
+    $("a").click(function() {
+        $("body").toggleClass("is-fade");
+    });
+});
+
+
+///////////墓場///////////
+
+// var nav = document.getElementsByTagName('body');
+// var button = document.getElementsByTagName('a');
+// document.getElementsByTagName('a').addEventListener('click', (event) => {
+//     document.getElementsByTagName('body').classList.add('is-fade');
+// });
+
+// var a = document.querySelectorAll('a');
+
+// triggers.forEach(function(a) {
+//     a.addEventListener('click', function() {
+//         document.getElementsByTagName('body').classList.add('is-fade');
+//     });
+// });
+
+// let elements = document.getElementsByTagName("a");
+// Array.prototype.forEach.addEventListener("click", function (elements) {
+//     document.getElementsByTagName('body').classList.add("is-fade");
+// });
+
+// document.getElementsByTagName('body').addEventListener('click', () => {
+//     document.getElementsByTagName('a').classList.add('is-fade');
+// });
+
+// $(window).on('load', function(){
+//     $('body').removeClass(is-fade);
+//   });
